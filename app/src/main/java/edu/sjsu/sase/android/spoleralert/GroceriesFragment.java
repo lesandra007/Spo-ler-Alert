cb5c8d4e783ddec459c469c55ca99c1a2ff7f754
AssortedJellies <129990117+AssortedJellies@users.noreply.github.com>
2025-04-12 16:41:56 -0700

    Test commit for branching

--- app/src/main/java/edu/sjsu/sase/android/spoleralert/GroceriesFragment.java
+++ app/src/main/java/edu/sjsu/sase/android/spoleralert/GroceriesFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -75,6 +76,9 @@ public class GroceriesFragment extends Fragment {
         );
         gs_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         gs_dropdown.setAdapter(gs_adapter);
+
+        //create RecyclerView for displaying list
+        RecyclerView grocery_list = groceries_view.findViewById(R.id.grocery_list_area);
 
         //clicking on the bottom right plus button (to add groceries)
         groceries_view.findViewById(R.id.add_groceries_button).setOnClickListener(new View.OnClickListener() {
