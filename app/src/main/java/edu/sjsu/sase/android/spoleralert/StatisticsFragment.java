3c83ae0377b886c7da56813d1f4e695d564d2399
Anne Mai <anne.mai@sjsu.edu>
2025-04-14 01:35:52 -0700

    Added libraries to make bar graphs, and graphs in general. Made my first dummy graph!
    TODO: Make it based on user input or database

--- app/src/main/java/edu/sjsu/sase/android/spoleralert/StatisticsFragment.java
+++ app/src/main/java/edu/sjsu/sase/android/spoleralert/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package edu.sjsu.sase.android.spoleralert;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,82 +11,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatisticsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class StatisticsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StatisticsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatisticsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StatisticsFragment newInstance(String param1, String param2) {
-        StatisticsFragment fragment = new StatisticsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ArrayList barArraylist;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View statistics_view = inflater.inflate(R.layout.fragment_statistics, container, false);
-        NavController controller = NavHostFragment.findNavController(this);
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
+        BarChart barChart = view.findViewById(R.id.barchart);
+        getData();
+        BarDataSet barDataSet = new BarDataSet(barArraylist, "Spo!ler Alert Statistics");
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(true);
+        return view;
+    }
 
-        //clicking on the buttons in the bottom bar to go to the different main parts of the app
-        statistics_view.findViewById(R.id.grocery_list_bottom_bar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                controller.navigate(R.id.groceriesFragment);
-            }
-        });
-
-        statistics_view.findViewById(R.id.recipes_bottom_bar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                controller.navigate(R.id.recipesFragment);
-            }
-        });
-
-        statistics_view.findViewById(R.id.statistics_bottom_bar).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                controller.navigate(R.id.statisticsFragment);
-            }
-        });
-
-        return statistics_view;
+    private void getData() {
+        barArraylist = new ArrayList();
+        barArraylist.add(new BarEntry(2f, 10));
+        barArraylist.add(new BarEntry(3f, 20));
+        barArraylist.add(new BarEntry(4f, 30));
+        barArraylist.add(new BarEntry(5f, 40));
+        barArraylist.add(new BarEntry(6f, 50));
     }
 }
